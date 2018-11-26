c9443be661e3e00d4d594853ea7be58f61d7e903
tcheng <tcheng10@illinois.edu>
2018-11-26 15:36:40 -0600

    Frontend dev merge for report (#7)
    * Checkpoint 1
    
    * Checkpoint 2

--- frontend/js/vis.js
+++ frontend/js/vis.js
@@ -1,55 +1,118 @@
-d3.json("https://gist.githubusercontent.com/dmesquita/41d02eb763f52158990e89b0e4691ada/raw/654125c9c41d9b5be0a0153465b1f767ffcf4687/dataset_final.json", function(error, data) {
-            chart(data.projects);
+// Asks JavaScript to show more errors.
+"use strict";
+
+
+
+$(function() {
+    $.getJSON("js/projects.json")
+        .done(function(data) { visualize(data); })
+        .fail(function() { alert("Failed to load the JSON file!\n(Did your Python run?)"); });
 });
-function chart(data) {
-    var dateFormat = d3.timeParse("%d/%m/%Y");
-    var timeScale = d3.scaleTime()
-                   .domain(d3.extent(data, d => dateFormat(d.date)))
-                   .range([0, 1000]);
-  
-    let dataByDates = d3.nest().key(d => d.date).entries(data);
-    let tickValues = dataByDates.map(d => dateFormat(d.key));
-  
-    let dataByCategories = d3.nest().key(d => d.status).entries(data);
-    let categories = dataByCategories.map(d => d.key).sort();
-    let colorScale = d3.scaleLinear()
-                 .domain([0, categories.length])
-                 .range(["#00B9FA", "#F95002"])
-                 .interpolate(d3.interpolateHcl);
-  
-    var xAxis = d3.axisBottom()
-                .scale(timeScale)
-                .tickValues(tickValues)
-                .tickSize(250, 0, 0)
-                .tickSizeOuter(0);
-    var grid = d3.select("svg").append('g').call(xAxis);
-  
-    yScale = d3.scaleLinear().domain([0, data.length]).range([0, 250]);
-  
-    var projects = d3.select("svg")
-                   .append('g')
-                   .selectAll("this_is_empty")
-                   .data(data)
-                   .enter();
-  
-    var barWidth = 140;
-  
-    var innerRects = projects.append("rect")
-                  .attr("rx", 3)
-                  .attr("ry", 3)
-                  .attr("x", (d,i) => timeScale(dateFormat(d.date)) - barWidth/2)
-                  .attr("y", (d,i) => yScale(i))
-                  .attr("width", barWidth)
-                  .attr("height", 30)
-                  .attr("stroke", "none")
-                  .attr("fill", d => d3.rgb(colorScale(categories.indexOf(d.status))));
-  
-    var rectText = projects.append("text")
-                  .text(d => d.label)
-                  .attr("x", d => timeScale(dateFormat(d.date)))
-                  .attr("y", (d,i) => yScale(i) + 20)
-                  .attr("font-size", 11)
-                  .attr("text-anchor", "middle")
-                  .attr("text-height", 30)
-                  .attr("fill", "#fff"); 
-}
+
+
+var visualize = function(data) {
+ var parseDate = d3.time.format("%H-%M-%S").parse;
+
+    data.forEach(function(d) {
+        d.from = parseDate(d.from);
+        d.to = parseDate(d.to);
+    });
+    var margin = {top: 50, right: 50, bottom: 50, left: 150},
+        width = 2000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], .2);
+
+    var x = d3.time.scale().range([0, width]);
+
+    y.domain(data.map(function(d) { return d.activity; }));
+    x.domain([d3.min(data,function(d){return d.from;}), d3.max(data,function(d){return d.to;})]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(30)
+        .tickFormat(d3.time.format("%H:%M"));
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var svg = d3.select("#container").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+          .append("text")
+          .attr("x", width-margin.right)
+          .attr("dx", ".71em")
+          .attr("dy", "-0.2em")
+          .text("Time");
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
+
+      svg.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("y", function(d) { return y(d.activity); })
+          .attr("height", "20")
+          .attr("fill",function(d) {
+            if(d.task == "on") {
+              return "#ffcc2e";
+            }
+            else if(d.task == "off") {
+              return "#3498df";
+            }
+            else {
+              return "#444";
+            }
+          })
+          .attr("transform", "translate(0,10)")
+          .attr("x", function(d) { return x(d.from); })
+          .attr("width", function(d) { return x(d.to) - x(d.from)});
+
+
+    var tooltip = d3.select("#container")
+    .append('div')
+    .attr('class', 'tooltip');
+
+    tooltip.append('div').attr('class', 'activity');
+    tooltip.append('div').attr('class', 'task')
+    tooltip.append('div').attr('class', 'tempRange');
+    // tooltip.append('div').attr('class', 'progress');
+
+    svg.selectAll(".bar,.pending")
+    .on('mouseover', function(d) {
+
+      tooltip.select('.activity').html("<b>" + d.activity + "</b>");
+      tooltip.select('.activity').html("<b>" + d.activity + "</b>");
+      tooltip.select('.tempRange').html(d.from.toDateString() + " to " + d.to.toDateString());
+      
+      // tooltip.select('.progress').html(d.progress + "% completed");
+
+      tooltip.style('display', 'block');
+      tooltip.style('opacity',2);
+
+    })
+    .on('mousemove', function(d) {
+      tooltip.style('top', (d3.event.layerY + 10) + 'px')
+      .style('left', (d3.event.layerX) + 'px');
+      console.log(d3.event.layerX);
+    })
+    .on('mouseout', function() {
+      tooltip.style('display', 'none');
+      tooltip.style('opacity',0);
+    });
+
+
+
+};
