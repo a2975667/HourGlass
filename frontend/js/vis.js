887d0d684ec5200636743f71b1a4194ff6b9079d
Jeffrey Tsai <jeffreytsai@jeffreys-mbp.lan>
2018-11-28 07:33:21 +0800

    calendar visualization

--- frontend/js/vis.js
+++ frontend/js/vis.js
@@ -11,6 +11,13 @@ $(function() {
         .fail(function() { alert("Failed to load the JSON file!\n(Did your Python run?)"); });
 });
 
+$(function() {
+    $.getJSON("js/calData.json")
+        .done(function(data) { 
+        // localStorage.setItem('key',JSON.stringify(data));
+          visualizeCal(data); })
+        .fail(function() { alert("Failed to load the JSON file!\n(Did your Python run?)"); });
+});
 
 var visualize = function(data) {
  // var parseDate = d3.time.format("%H-%M-%S").parse;
@@ -115,7 +122,7 @@ var visualize = function(data) {
             //   return "#444";
             // }
           })
-          .attr("transform", "translate(0,16)")
+          .attr("transform", "translate(0,13)")
           .attr("x", function(d) { return x(d.from); })
           .attr("width", function(d) { return x(d.to) - x(d.from)});
 
@@ -134,6 +141,133 @@ var visualize = function(data) {
 
       tooltip.select('.name').html("<b>" + d.name + "</b>");
       tooltip.select('.name').html("<b>" + d.name + "</b>");
+      tooltip.select('.tempRange').html(d.from.toString().substr(4,20) + " TO " + d.to.toString().substr(4,20));
+      
+      // tooltip.select('.progress').html(d.progress + "% completed");
+
+      tooltip.style('display', 'block');
+      tooltip.style('opacity',2);
+
+    })
+    .on('mousemove', function(d) {
+      tooltip.style('top', (d3.event.layerY + 10) + 'px')
+      .style('left', (d3.event.layerX) + 'px');
+      console.log(d3.event.layerX);
+    })
+    .on('mouseout', function() {
+      tooltip.style('display', 'none');
+      tooltip.style('opacity',0);
+    });
+
+};
+
+
+var visualizeCal = function(data) {
+ // var parseDate = d3.time.format("%H-%M-%S").parse;
+    var parseDate2 = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
+
+    data.forEach(function(dd) {
+        dd.from = parseDate2(dd.from);
+        dd.to = parseDate2(dd.to);
+    });
+    var margin = {top: 50, right: 50, bottom: 50, left: 150},
+        width = 2000 - margin.left - margin.right,
+        height = 45;
+
+    var y2 = d3.scale.ordinal()
+        .rangeRoundBands([0, height], .1);
+
+    var x2 = d3.time.scale().range([0, width]);
+
+    y2.domain(data.map(function(dd) { return dd.name; }));
+    x2.domain([d3.min(data,function(dd){return dd.from;}), d3.max(data,function(dd){return dd.to;})]);
+
+    var xAxis2 = d3.svg.axis()
+        .scale(x2)
+        .orient("bottom")
+        .ticks(10).outerTickSize(0)
+        .tickSize(0)
+        .tickPadding(-5000)
+        // .attr("font-size","4em")
+        .tickFormat(d3.time.format("%H %p"));
+
+    var yAxis2 = d3.svg.axis()
+        .scale(y2)
+        .tickSize(0)
+        .tickPadding(10)
+        // .attr("fill","none")
+        .orient("left");
+
+    var svg = d3.select("#container").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .style('font-weight','bold')
+          .style('font-family','Raleway')
+          .style('text-transform','lowercase')
+          .attr("transform", "translate(0," + height + ")")
+          .call(customXAxis2)
+          .append("text")
+          .attr('fill','none')
+          .attr('stroke','#fff')
+          .attr("x", width-margin.right)
+          .attr("dx", ".71em")
+          .attr("dy", "-0.2em")
+          .text("Time");
+
+      function customYAxis2(g) {
+        var s = g.selection ? g.selection() : g;
+        g.call(yAxis2);
+        s.select(".domain").remove();
+        
+      }
+
+      function customXAxis2(g) {
+        var s = g.selection ? g.selection() : g;
+        g.call(xAxis2);
+        s.select(".domain").remove();
+        
+      }
+      svg.append("g")
+          .attr("class", "y axis")
+          .style('font-style', 'italic')
+          .style('font-family','Raleway')
+          .call(customYAxis2);
+
+      svg.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("y", function(dd) { return y2(dd.name); })
+          .attr("height", "20")
+          .attr("fill",function(d){
+            if(d.status == "busy") {
+              return "#3498df";
+            }
+          })
+          .attr("transform", "translate(0,8)")
+          .attr("x", function(dd) { return x2(dd.from); })
+          .attr("width", function(dd) { return x2(dd.to) - x2(dd.from)});
+
+
+    var tooltip = d3.select("#container")
+    .append('div')
+    .attr('class', 'tooltip');
+
+    tooltip.append('div').attr('class', 'name');
+    tooltip.append('div').attr('class', 'task')
+    tooltip.append('div').attr('class', 'tempRange');
+    // tooltip.append('div').attr('class', 'progress');
+
+    svg.selectAll(".bar,.pending")
+    .on('mouseover', function(d) {
+
+      tooltip.select('.name').html("<b>" + d.name + "</b>");
+      tooltip.select('.task').html("<b> BUSY </b>");
       tooltip.select('.tempRange').html(d.from.toString().substr(4,20) + " TO " + d.to.toString().substr(4,20));
       
       // tooltip.select('.progress').html(d.progress + "% completed");
