7fc5b698a4d7be20d2bf1aae1eb764e922cb32b1
kys2 <44933652+kys2@users.noreply.github.com>
2018-12-01 16:57:36 -0600

    Update date.js
    Change to get YESTERDAY.

--- frontend/js/date.js
+++ frontend/js/date.js
@@ -1,4 +1,5 @@
 function datetext() {
     var date = new Date();
+    date.setDate(date.getDate() - 1);
     document.getElementById("date_text").innerHTML = date.toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' });
 }
