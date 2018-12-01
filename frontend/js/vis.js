66c9bbc7abd79028339a5e8d2f509446b1ac023f
Jeffrey Tsai <jeffreytsai@jeffreys-mbp.lan>
2018-12-02 07:55:37 +0800

    Productivity bar Implementation

--- frontend/js/vis.js
+++ frontend/js/vis.js
@@ -12,8 +12,8 @@
 // PULLING FROM FILE
 
 var today = new Date();
-// var day = today.getDate()-1;
-var day = 27;
+var day = today.getDate()-1;
+// var day = 27;
 var day2= day+1;
 var month = today.getMonth()+1; //January is 0!
 var year = today.getFullYear();
@@ -25,20 +25,20 @@ if(day<10) {
 if(month<10) {
     month = '0'+month
 } 
-
 today = year + '-' + month + '-' + day;
 var today2 = year + '-' + month + '-' + day2;
+
 $.ajax({
   type: 'GET',
   url: 'https://hourglass-api.herokuapp.com/api/rank-distract-for-d3?start_date='+today+'&end_date='+today+'&n=5',
-  headers: { 'key': 'B63TuW7oKpBKsoZePpZ31IGZkxtEKuUVTcYQeJRz' },
+  headers: { 'key': 'B63mVDYNd_2h9n4dbHjgrjMyNBCjVdZUOH5luFCE' },
   success: function(data) {
     console.log(data);
     localStorage.setItem('key',JSON.stringify(data));
     visualize(data); 
   },
   error: function() {
-      alert('error pulling Visualization code from API');
+      alert('error pulling visualization data from API');
   }
 });
 
@@ -54,9 +54,28 @@ $.ajax({
     visualizeCal(data); 
   },
   error: function() {
-      alert('error pulling Calendar code from API');
+      alert('error pulling calendar data from API');
   }
 });
+
+
+$.ajax({
+  type: 'GET',
+  url: 'https://hourglass-api.herokuapp.com/api/pro-nonpro-time?start_date='+today+'&end_date='+today,
+  headers: { 
+    "Key": "B63mVDYNd_2h9n4dbHjgrjMyNBCjVdZUOH5luFCE",
+  },
+  success: function(data) {
+    console.log(data);
+    visualizeProductivity(data); 
+  },
+  error: function() {
+      alert('error pulling productivity data from API');
+  }
+});
+
+
+// https://hourglass-api.herokuapp.com/api/pro-nonpro-time?start_date=2018-12-01&end_date=2018-12-01
 
 // $(function() {
 //     $.getJSON("js/calData.json")
@@ -215,6 +234,131 @@ var visualize = function(data) {
 
 };
 
+var visualizeProductivity = function(data) {
+ // var parseDate = d3.time.format("%H-%M-%S").parse;
+    var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S").parse;
+
+    data.forEach(function(d) {
+        d.from = parseDate(d.from);
+        d.to = parseDate(d.to);
+    });
+    var margin = {top: 0, right: 50, bottom: 50, left: 180},
+        width = 1500 - margin.left - margin.right,
+        height = 80;
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([0, height], .1);
+
+    var x = d3.time.scale().range([0, width]);
+
+    y.domain(data.map(function(d) { return d.name; }));
+    x.domain([d3.min(data,function(d){return d.from;}), d3.max(data,function(d){return d.to;})]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(10).outerTickSize(0)
+        .tickSize(0)
+        .tickPadding(-5000)
+        // .attr("font-size","4em")
+        .tickFormat(d3.time.format("%H %p"));
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickSize(0)
+        .tickPadding(10)
+        // .attr("fill","none")
+        .orient("left");
+
+    var svg = d3.select("#index_productivity_graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .style('font-weight','bold')
+          .style('font-family','Raleway')
+          .style('text-transform','lowercase')
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+          .append("text")
+          .attr("x", width-margin.right)
+          .attr("dx", ".71em")
+          .attr("dy", "-0.2em")
+
+          .text("Time");
+
+      function customYAxis(g) {
+        var s = g.selection ? g.selection() : g;
+        g.call(yAxis);
+        s.select(".domain").remove();
+        
+      }
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .style('font-style', 'italic')
+          .style('font-family','Raleway')
+          .call(customYAxis);
+
+      svg.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("y", function(d) { return y(d.name); })
+          .attr("height", "10")
+          .attr("fill",function(d) {
+            if(d.name == "productive") {
+              return "#3498df";
+            }
+            else if(d.name == "unproductive") {
+              return "#ffcc2e";
+            }
+            // else {
+            //   return "#444";
+            // }
+          })
+          .attr("transform", "translate(0,13)")
+          .attr("x", function(d) { return x(d.from); })
+          .attr("width", function(d) { return x(d.to) - x(d.from)});
+
+
+    var tooltip = d3.select("#index_gantt_graph")
+    .append('div')
+    .attr('class', 'tooltip');
+
+    tooltip.append('div').attr('class', 'name');
+    tooltip.append('div').attr('class', 'category')
+    tooltip.append('div').attr('class', 'ran');
+    // tooltip.append('div').attr('class', 'progress');
+
+    svg.selectAll(".bar,.pending")
+    .on('mouseover', function(d) {
+
+      tooltip.select('.name').html("Site: <b>" + d.name + "</b>");
+      tooltip.select('.category').html("Category: <b>" + d.category + "</b>");
+      tooltip.select('.ran').html(d.from.toString().substr(4,20) + " TO " + d.to.toString().substr(4,20));
+      
+      // tooltip.select('.progress').html(d.progress + "% completed");
+
+      tooltip.style('display', 'block');
+      tooltip.style('opacity',2);
+
+    })
+    .on('mousemove', function(d) {
+      tooltip.style('top', (d3.event.layerY + 10) + 'px')
+      .style('left', (d3.event.layerX) + 'px');
+      console.log(d3.event.layerX);
+    })
+    .on('mouseout', function() {
+      tooltip.style('display', 'none');
+      tooltip.style('opacity',0);
+    });
+
+};
+
 
 var visualizeCal = function(data) {
  // var parseDate = d3.time.format("%H-%M-%S").parse;
@@ -224,7 +368,7 @@ var visualizeCal = function(data) {
         dd.from = parseDate2(dd.start_time);
         dd.to = parseDate2(dd.end_time);
     });
-    var margin = {top: 50, right: 50, bottom: 20, left: 180},
+    var margin = {top: 130, right: 50, bottom: 20, left: 180},
         width = 1500 - margin.left - margin.right,
         height = 80;
 
@@ -288,7 +432,7 @@ var visualizeCal = function(data) {
       }
       svg.append("g")
           .attr("class", "y axis")
-          .style('font-style', 'italic')
+          .style('font-weight', 'bold')
           .style('font-family','Raleway')
           .call(customYAxis2);
 
@@ -300,10 +444,10 @@ var visualizeCal = function(data) {
           .attr("height", "10")
           .attr("fill",function(d){
             if(d.status == "busy") {
-              return "#3498df";
+              return "#555";
             }
             else{
-              return "#ffcc2e";
+              return "#ddd";
             }
           })
           .attr("transform", "translate(0,11)")
